d2df3c87a438bf202ae5dc2d4b234bc4199395f7
Elmir Ismayilzade <devkryternext@gmail.com>
2021-09-04 15:11:03 +0400

    feat: update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,11 +10,31 @@ module.exports = {
   },
   extends: [
     '@nuxtjs',
-    'plugin:nuxt/recommended',
-    'prettier'
+    'plugin:nuxt/recommended'
   ],
   plugins: [
   ],
   // add your custom rules here
-  rules: {}
+  rules: {
+    'vue/component-name-in-template-casing': [
+      'error',
+      'PascalCase',
+      { registeredComponentsOnly: false }
+    ],
+    'vue/max-attributes-per-line': [
+      'error',
+      {
+        singleline: 1,
+        multiline: {
+          max: 1,
+          allowFirstLine: false
+        }
+      }
+    ],
+    'arrow-parens': 0,
+    'no-console': 0,
+    'import/order': 0,
+    indent: ['error', 2, { ignoredNodes: ['TemplateLiteral'] }],
+    'template-curly-spacing': 'off'
+  }
 }
