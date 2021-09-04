96ecd46612357810f2be3f93e8f63901e1ac6c29
Elmir Ismayilzade <devkryternext@gmail.com>
2021-09-04 18:11:47 +0400

    feat(footer): add language switcher

--- components/core/TheFooter.vue
+++ components/core/TheFooter.vue
@@ -1,7 +1,26 @@
 <template>
   <footer class="footer">
     <div class="app-container">
-      {{ $t('common.copyright', {year: currentYear}) }}
+      <div class="footer__left">
+        <span>
+          {{ $t('common.copyright', {year: currentYear}) }}
+        </span>
+        <div class="change-language">
+          <select
+            v-model="locale"
+            @change="onLanguageSwitch"
+          >
+            <option
+              v-for="l in appLocales"
+              :key="l.code"
+              :value="l.code"
+            >
+              {{ l.code }}
+            </option>
+          </select>
+        </div>
+      </div>
+      <div class="footer__right" />
     </div>
   </footer>
 </template>
@@ -9,14 +28,40 @@
 <script>
 export default {
   name: 'TheFooter',
+
+  data () {
+    return {
+      locale: this.$i18n.locale
+    }
+  },
+
   computed: {
     currentYear () {
       return new Date().getFullYear()
+    },
+    appLocales () {
+      return this.$i18n.locales
+    }
+  },
+
+  methods: {
+    onLanguageSwitch () {
+      this.$i18n.setLocale(this.locale)
     }
   }
 }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+.footer {
+  &__left {
+    display: flex;
+    .change-language {
+      margin-left: 30px;
+      select {
+        outline: none;
+      }
+    }
+  }
+}
 </style>
