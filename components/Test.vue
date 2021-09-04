8f9e709eb600bd11f92a55c133e286180c45aeb7
Elmir Ismayilzade <devkryternext@gmail.com>
2021-09-04 14:05:02 +0400

    feat: project initialization

---
+++ components/Test.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1 class="title">
+    Hello world
+  </h1>
+</template>
+
+<script>
+export default {
+  name: "Test"
+}
+</script>
+
+<style lang="scss" scoped>
+.title {
+  text-transform: uppercase;
+  font-size: 50px;
+  font-family: sans-serif;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <Test />
+</template>
+
+<script>
+import Test from "~/components/Test.vue";
+
+export default {
+  name: 'IndexPage',
+
+  components: {
+    Test
+  }
+}
+</script>
