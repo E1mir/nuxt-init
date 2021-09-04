25a3568e194cf5a52bed3639d727f9f9aee4bd69
Elmir Ismayilzade <devkryternext@gmail.com>
2021-09-04 18:11:23 +0400

    chore(Welcome): add smile icon

--- components/Welcome.vue
+++ components/Welcome.vue
@@ -3,6 +3,13 @@
     <div class="app-container">
       <h1 class="title">
         {{ $t('common.welcome') }}
+        <Unicon
+          class="title__icon"
+          name="smile-beam"
+          height="40"
+          width="40"
+          hover-fill="#00DC82"
+        />
       </h1>
     </div>
   </section>
@@ -16,8 +23,14 @@ export default {
 
 <style lang="scss" scoped>
 .title {
+  display: flex;
+  align-items: center;
   text-transform: uppercase;
   font-size: 50px;
   margin-bottom: $spacing-md;
+  &__icon {
+    margin-left: 5px;
+    cursor: pointer;
+  }
 }
 </style>
